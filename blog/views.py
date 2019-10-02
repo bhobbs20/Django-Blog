92d6a5201bdd5c814d35a24c82a87ac320d31874
Brian Hobbs <brianhobbs216@gmail.com>
2019-10-02 15:46:54 -0400

    cleaned up index with partials

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,11 @@ from .models import Post
 
 def index(request):
     posts = Post.objects.order_by('-created_date').filter(is_published=True)
-   
+    popular_posts = [Post.objects.get(pk=1), Post.objects.get(pk=2), Post.objects.get(pk=3)]
+    
     context = {
         'posts': posts,
-       
+        'popular_posts': popular_posts,
     }
 
     return render(request, 'blogs/index.html', context)
