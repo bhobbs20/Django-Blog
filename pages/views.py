855fb6feb9c8925dae66ad981ace3fe77cc2bc0c
Brian Hobbs <brianhobbs216@gmail.com>
2019-10-03 12:45:29 -0400

    add some stuff to home

--- pages/views.py
+++ pages/views.py
@@ -8,12 +8,13 @@ def index(request):
     posts = Post.objects.order_by('-created_date').filter(is_published=True)
     cats = Category.objects.all()
     popular_posts = [Post.objects.get(pk=1), Post.objects.get(pk=2), Post.objects.get(pk=3)]
+
    
     context = {
         'posts': posts,
         'cats': cats,
         'popular_posts': popular_posts,
-        
+      
     }
 
     return render(request, 'pages/index.html', context)
