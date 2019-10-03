d6c0a1cf2761bae522b1ef1e9e3fb00df6eae76b
Brian Hobbs <brianhobbs216@gmail.com>
2019-10-03 15:48:29 -0400

    styled and added data to sinle post page

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage,PageNotAnInteger, Paginator
 from .models import Post
+from categories.models import Category
 
 
 def index(request):
@@ -17,9 +18,12 @@ def index(request):
 
 def blog(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-
+    popular_posts = [Post.objects.get(pk=1), Post.objects.get(pk=2), Post.objects.get(pk=3)]
+    cats = Category.objects.all() 
     context = {
-       'post': post
+       'post': post,
+       'cats': cats,
+       'popular_posts': popular_posts,
     }
 
     return render(request, 'blogs/blog.html', context)
