8a0d5920152df933d968eb25f832b6ed4dcf6a23
Brian Hobbs <brianhobbs216@gmail.com>
2019-10-03 13:17:49 -0400

    Implemented pagination for posts

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from blog.models import Post
 from categories.models import Category
+
+
 
 
 def index(request):
@@ -9,9 +12,12 @@ def index(request):
     cats = Category.objects.all()
     popular_posts = [Post.objects.get(pk=1), Post.objects.get(pk=2), Post.objects.get(pk=3)]
 
+    paginator = Paginator(posts, 3)
+    page = request.GET.get('page')
+    paged_posts = paginator.get_page(page)
    
     context = {
-        'posts': posts,
+        'posts': paged_posts,
         'cats': cats,
         'popular_posts': popular_posts,
       
